d7716a03338eccad87f0eae87ecfc995eb60d8e1
bogaclau <sebba30015boga@gmail.com>
2023-08-23 18:19:12 -0300

    tipos de datos

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("hello world");
+// tipos de datos primitivos
+
+// string
+console.log(typeof "Juan Lavayen");
+
+// number
+console.log(typeof 123123);
+
+// bigint
+console.log(typeof 490528730495872039847590823746n);
+
+// booleans
+console.log(typeof true);
+console.log(typeof false);
+
+// null
+console.log(typeof null);
+
+// undefined
+console.log(typeof undefined);
+
+// symbol
+console.log(typeof Symbol());
+////////////////////////////////////////////////
+// tipos de datos compuestos
+
+// array
+console.log(["hola", true, 12, 12n, Symbol()]);
+
+// objects
+console.log({
+  nombre: "Luis",
+  apellido: "Marin",
+});
